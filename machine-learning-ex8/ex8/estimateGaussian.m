9aa5201734f73eb6d9a638023c1cfd573d0ce38d
Julia Hu <juliayhu@gmail.com>
2018-06-13 01:15:22 -0700

    last one

---
+++ machine-learning-ex8/ex8/estimateGaussian.m
@@ -0,0 +1,37 @@
+function [mu sigma2] = estimateGaussian(X)
+%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%Gaussian distribution using the data in X
+%   [mu sigma2] = estimateGaussian(X), 
+%   The input X is the dataset with each n-dimensional data point in one row
+%   The output is an n-dimensional vector mu, the mean of the data set
+%   and the variances sigma^2, an n x 1 vector
+% 
+
+% Useful variables
+[m, n] = size(X);
+
+% You should return these values correctly
+mu = zeros(n, 1);
+sigma2 = zeros(n, 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the mean of the data and the variances
+%               In particular, mu(i) should contain the mean of
+%               the data for the i-th feature and sigma2(i)
+%               should contain variance of the i-th feature.
+%
+
+% X is 307x2, mu is 2x1 
+mu = (1/m) .* sum(X);
+
+% sigma2 is nx1 vector
+sub = bsxfun(@minus, X, mu);
+sqr = sub .^2;
+sigma2 = (1/m) .* sum(sqr);
+
+mu = mu';
+sigma2 = sigma2';
+% =============================================================
+
+
+end
