0b30d27b28340d0b1cc83f9343a96d93538ffae5
Julia Hu <juliayhu@gmail.com>
2018-06-12 22:12:12 -0700

    comments and wk 5

--- machine-learning-ex4/ex4/sigmoidGradient.m
+++ machine-learning-ex4/ex4/sigmoidGradient.m
@@ -5,7 +5,7 @@ function g = sigmoidGradient(z)
 %   evaluated at z. This should work regardless if z is a matrix or a
 %   vector. In particular, if z is a vector or matrix, you should return
 %   the gradient for each element.
-
+%   Initialize
 g = zeros(size(z));
 
 % ====================== YOUR CODE HERE ======================
